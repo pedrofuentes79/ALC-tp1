f426b8791eecc968f66627dd6f82d69e77f48b41
pfuentes <pedrofuentes7799@gmail.com>
2025-04-15 13:50:21 -0300

    agrega calculo del pagerank

--- tp1/template_funciones.py
+++ tp1/template_funciones.py
@@ -70,10 +70,13 @@ def calcula_pagerank(A,alfa):
     # d: coeficientes de damping
     # Retorna: Un vector p con los coeficientes de page rank de cada museo
     C = calcula_matriz_C(A)
-    N = ... # Obtenemos el número de museos N a partir de la estructura de la matriz A
-    M = ...
+    N = A.shape[0] 
+    M = (N/alfa) * (np.identity(N) - (1-alfa)*C)
     L, U = calculaLU(M) # Calculamos descomposición LU a partir de C y d
-    b = ... # Vector de 1s, multiplicado por el coeficiente correspondiente usando d y N.
+    
+    # TODO: que es el coeficiente con d???
+    b = np.ones(N) # Vector de 1s, multiplicado por el coeficiente correspondiente usando d y N.
+
     Up = scipy.linalg.solve_triangular(L,b,lower=True) # Primera inversión usando L
     p = scipy.linalg.solve_triangular(U,Up) # Segunda inversión usando U
     return p
